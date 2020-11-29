41075ec1b3610627f63c10302ff2f1e8b82ca514
Nicole <nicolemamay4@gmail.com>
2020-11-29 15:13:35 -0500

    add setup to hero test

--- src/components/Hero/Hero.test.js
+++ src/components/Hero/Hero.test.js
@@ -7,45 +7,30 @@ import ThemeProviderMock from '__mocks__/ThemeProviderMock';
 import { PureHero as Hero } from './Hero';
 
 describe('Hero', () => {
-  it('renders default', () => {
-    // Created using the query from Hero.js
+  const setup = ({ isIntroDone }) => {
     const mockData = {
       greetings: 'Test greeting,',
       subtitle: 'Test subtitle',
       title: 'Test title',
-      isIntroDone: true,
     };
 
     const { container } = render(
-      <ContextProviderMock isIntroDone={false}>
+      <ContextProviderMock isIntroDone={isIntroDone}>
         <ThemeProviderMock>
-          <Hero {...mockData} />
+          <Hero isIntroDone={isIntroDone} {...mockData} />
         </ThemeProviderMock>
       </ContextProviderMock>,
     );
 
+    return { container };
+  };
+  it('renders default', () => {
+    const { container } = setup({ isIntroDone: true });
     expect(container).toMatchSnapshot();
   });
-});
 
-describe('Hero', () => {
   it('renders splash screen', () => {
-    // Created using the query from Hero.js
-    const mockData = {
-      greetings: 'Test greeting,',
-      subtitle: 'Test subtitle',
-      title: 'Test title',
-      isIntroDone: false,
-    };
-
-    const { container } = render(
-      <ContextProviderMock isIntroDone={true}>
-        <ThemeProviderMock>
-          <Hero {...mockData} />
-        </ThemeProviderMock>
-      </ContextProviderMock>,
-    );
-
+    const { container } = setup({ isIntroDone: false });
     expect(container).toMatchSnapshot();
   });
 });
